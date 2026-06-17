ff8bc94bc2dd0679d887b21ad2c0867b4d29a157
Samuel8812 <samuelbardelicanal@gmail.com>
2026-06-17 14:24:28 -0300

    aula4 finalizado(pagina 62)

--- pokedex/app/index.jsx
+++ pokedex/app/index.jsx
@@ -1,7 +1,7 @@
 import { Image, View, Text, ScrollView, TouchableOpacity } from "react-native";
-import { POKEMON_TYPE_COLORS } from "../../utils/colors";
+import { POKEMON_TYPE_COLORS } from "../utils/colors";
 import { styles } from "./styles";
-import Button from "../Button";
+import Button from "../components/Button";
 import { useRouter } from "expo-router";
 
 export default function Card({ pokemon }) {
